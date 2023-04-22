ece2486022e97377b93d76011385349066fc02f9
oTalDoHud <hudson.lucas.vieira@gmail.com>
2023-04-22 20:41:54 -0300

    slug auto

--- produto/models.py
+++ produto/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from PIL import Image
 from django.conf import settings
+from django.utils.text import slugify
 import os
 
 
@@ -10,22 +11,34 @@ class Produto(models.Model):
     descricao_longa = models.TextField()
     imagem = models.ImageField(
         upload_to="produto_imagens/%Y/%m", blank=True, null=True)
-    slug = models.SlugField(unique=True)
-    preco_marketing = models.FloatField()
-    preco_marketing_promocional = models.FloatField(default=0)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+    preco_marketing = models.FloatField(verbose_name="Preço")
+    preco_marketing_promocional = models.FloatField(default=0, verbose_name="Preço Promo")
     tipo = models.CharField(
         default="v",
         max_length=1,
         choices=(
-            ("V", "Variação"),
+            ("V", "Variável"),
             ("S", "Simples"),
         )
     )
+
+    def get_preco_formatado(self):
+        return f"R$: {self.preco_marketing:.2f}".replace(".", ",")
+    get_preco_formatado.short_description = "Preço"
+
+    def get_preco_promo_formatado(self):
+        return f"R$: {self.preco_marketing_promocional:.2f}".replace(".", ",")
+    get_preco_promo_formatado.short_description = "Preço promo"
 
     def __str__(self) -> str:
         return self.nome
 
     def save(self, *args, **kwargs):
+        if not self.slug:
+            slug = f"{slugify(self.nome)}"
+            self.slug = slug
+
         super().save(*args, **kwargs)
 
         max_image_size = 800
