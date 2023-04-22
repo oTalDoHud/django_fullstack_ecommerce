969dbbaf59b55e5eedffea2701cb00b4e60bf478
oTalDoHud <hudson.lucas.vieira@gmail.com>
2023-04-22 20:04:10 -0300

    Perfil adicionado

--- perfil/models.py
+++ perfil/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.forms import ValidationError
+from utils.validador_cpf import validar_cpf
+import re
 
-# Create your models here.
+
+class Perfil(models.Model):
+    Usuario = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="Usuário")
+    idade = models.PositiveBigIntegerField()
+    data_nascimento = models.DateField()
+    cpf = models.CharField(max_length=11)
+
+    endereco = models.CharField(max_length=50)
+    numero_endereco = models.CharField(max_length=5)
+    complemento_endereco = models.CharField(max_length=30)
+    bairro_endereco = models.CharField(max_length=30)
+    cep_endereco = models.CharField(max_length=8)
+    cidade = models.CharField(max_length=30)
+    estado = models.CharField(
+        max_length=2,
+        default="SP",
+        choices=(
+            ('AC', 'Acre'),
+            ('AL', 'Alagoas'),
+            ('AP', 'Amapá'),
+            ('AM', 'Amazonas'),
+            ('BA', 'Bahia'),
+            ('CE', 'Ceará'),
+            ('DF', 'Distrito Federal'),
+            ('ES', 'Espírito Santo'),
+            ('GO', 'Goiás'),
+            ('MA', 'Maranhão'),
+            ('MT', 'Mato Grosso'),
+            ('MS', 'Mato Grosso do Sul'),
+            ('MG', 'Minas Gerais'),
+            ('PA', 'Pará'),
+            ('PB', 'Paraíba'),
+            ('PR', 'Paraná'),
+            ('PE', 'Pernambuco'),
+            ('PI', 'Piauí'),
+            ('RJ', 'Rio de Janeiro'),
+            ('RN', 'Rio Grande do Norte'),
+            ('RS', 'Rio Grande do Sul'),
+            ('RO', 'Rondônia'),
+            ('RR', 'Roraima'),
+            ('SC', 'Santa Catarina'),
+            ('SP', 'São Paulo'),
+            ('SE', 'Sergipe'),
+            ('TO', 'Tocantins'),
+        )
+    )
+
+    def __str__(self) -> str:
+        return f"{self.Usuario}"
+
+    def clean(self) -> None:
+        error_massages = {}
+        if not validar_cpf(self.cpf):
+            error_massages["cpf"] = "Digite um CPF válido"
+
+        if re.search(r"[^0-9]", self.cep_endereco) or len(self.cep_endereco) < 8:
+            error_massages["cep_endereco"] = "Digite um CEP válido"
+
+        if error_massages:
+            raise ValidationError(error_massages)
+
+    class Meta:
+        verbose_name = "Pefil"
+        verbose_name_plural = "Perfis"
---
+++ utils/validador_cpf.py
@@ -0,0 +1,31 @@
+def validar_cpf(cpf: str) -> bool:
+    """Valida um CPF. Retorna True se válido, False caso contrário."""
+    
+    # Remove caracteres que não são dígitos
+    cpf = ''.join(filter(str.isdigit, cpf))
+
+    # Verifica se o CPF possui 11 dígitos
+    if len(cpf) != 11:
+        return False
+    
+    # Verifica se o CPF tem todos os números iguais
+    if cpf == cpf[0] * 11:
+        return False
+    
+    # Calcula o primeiro dígito verificador
+    soma = sum(int(cpf[i]) * (10 - i) for i in range(9))
+    resto = (soma * 10) % 11
+    if resto == 10:
+        resto = 0
+    if resto != int(cpf[9]):
+        return False
+    
+    # Calcula o segundo dígito verificador
+    soma = sum(int(cpf[i]) * (11 - i) for i in range(10))
+    resto = (soma * 10) % 11
+    if resto == 10:
+        resto = 0
+    if resto != int(cpf[10]):
+        return False
+    
+    return True
